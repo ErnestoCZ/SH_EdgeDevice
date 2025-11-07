e2d0028cffa3a8e063097cc10d774f39bfe80a92
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-11-07 08:48:00 +0100

    added new thread which is responsible to read the adc  and adjusted the project configs

--- src/main.c
+++ src/main.c
@@ -6,24 +6,52 @@
 #include <zephyr/bluetooth/bluetooth.h>
 
 LOG_MODULE_REGISTER(Smarthome_EdgeDevice, LOG_LEVEL_INF);
-
 #include "cadc.h"
 #include "cgatt.h"
 #include "customCPP/filter.h"
+
+void adc_thread_routine(void){
+	k_msleep(2000);
+	initADC();
+
+	for(;;){
+		readADC();
+		k_msleep(5000);
+	}
+}
+
+K_THREAD_DEFINE(adc_thread_id,512, adc_thread_routine,NULL ,NULL ,NULL , 7, K_USER, 0);
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,(sizeof(CONFIG_BT_DEVICE_NAME)-1)),
+};
+static const struct bt_le_adv_param adv_param[] = BT_LE_ADV_NCONN;
+
 int main(void)
 {
         LOG_INF("Device is starting...");
 
-	initADC();
+	// initADC();
 
 
-        // ret = bt_enable(NULL);
+	int ret = 0;
 
-	while(1){
-		readADC();
+    ret = bt_enable(NULL);
+
+	ret = bt_le_adv_start(adv_param,&ad,ARRAY_SIZE(ad),NULL,NULL);
+	if(ret){
+		LOG_ERR("Something gone wrong by start advertisements");
+	}
+
+	
+
+	// while(1){
+		// readADC();
 		//TODO add filter capabilities
-		k_msleep(5000);
-	};
+		// k_msleep(5000);
+		// k_yield();
+	// };
 
 
         return 0;
