fbcebfadb4d6e5eed4677490fbc07d3c146f3629
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-11-08 00:12:14 +0100

    formatted

--- src/custom/cadc.h
+++ src/custom/cadc.h
@@ -1,23 +1,21 @@
 #ifndef CADC_H
 #define CADC_H
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
+#include "cstructs.h"
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/adc.h>
-#include "cstructs.h"
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 // LOG_MODULE_REGISTER(CADC, LOG_LEVEL_DBG);
 
-//Structures
-static const struct adc_dt_spec adc_channel_temperature = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+// Structures
+static const struct adc_dt_spec adc_channel_temperature
+    = ADC_DT_SPEC_GET (DT_PATH (zephyr_user));
 
-//Functions
-void initADC(void);
-void readADC(void);
+// Functions
+void initADC (void);
+void readADC (void);
 
-void convert_mv_to_temp_LM35(const int32_t *val_mv, int32_t *val_temp);
-
-
+void convert_mv_to_temp_LM35 (const int32_t *val_mv, int32_t *val_temp);
 
 #endif
-
