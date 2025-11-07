356fec06b221b97b769d12495d25e9d6426258a4
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-11-08 00:14:33 +0100

    cleaned up files and added adv param and scan response BLE array

--- src/main.c
+++ src/main.c
@@ -4,13 +4,56 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
 
 LOG_MODULE_REGISTER(Smarthome_EdgeDevice, LOG_LEVEL_INF);
 #include "cadc.h"
 #include "cgatt.h"
 #include "customCPP/filter.h"
 
-void adc_thread_routine(void){
+//EXTERN
+extern struct bt_conn_cb bt_connection_callbacks;
+
+//PRIVATE FUNCTION PROTOTYPES
+void adc_thread_handler(void);
+
+
+//STATIC THREAD DEFINES
+K_THREAD_DEFINE(adc_thread_id,512, adc_thread_handler,NULL ,NULL ,NULL , 7, K_USER, 0);
+
+//BLE ADVERTISEMENTS AND RESPONSES
+const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,(sizeof(CONFIG_BT_DEVICE_NAME)-1)),
+};
+const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_TEMPERATURE_VAL)),
+};
+const struct bt_le_adv_param adv_param[] = BT_LE_ADV_PARAM((BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_SCANNABLE),
+																	BT_GAP_ADV_SLOW_INT_MIN,
+																	BT_GAP_ADV_SLOW_INT_MAX,
+																	NULL);
+
+int main(void)
+{
+    LOG_INF("Device is starting...");
+	int ret = 0;
+
+    ret = bt_enable(NULL);
+
+	bt_conn_cb_register(&bt_connection_callbacks);
+
+	ret = bt_le_adv_start(adv_param,&ad,ARRAY_SIZE(ad),&sd,ARRAY_SIZE(sd));
+	if(ret){
+		LOG_ERR("Something gone wrong by start advertisements");
+	}
+
+        return 0;
+}
+
+
+//FUNCTION DEFINITIONS
+void adc_thread_handler(void){
 	k_msleep(2000);
 	initADC();
 
@@ -20,39 +63,3 @@ void adc_thread_routine(void){
 	}
 }
 
-K_THREAD_DEFINE(adc_thread_id,512, adc_thread_routine,NULL ,NULL ,NULL , 7, K_USER, 0);
-
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,(sizeof(CONFIG_BT_DEVICE_NAME)-1)),
-};
-static const struct bt_le_adv_param adv_param[] = BT_LE_ADV_NCONN;
-
-int main(void)
-{
-        LOG_INF("Device is starting...");
-
-	// initADC();
-
-
-	int ret = 0;
-
-    ret = bt_enable(NULL);
-
-	ret = bt_le_adv_start(adv_param,&ad,ARRAY_SIZE(ad),NULL,NULL);
-	if(ret){
-		LOG_ERR("Something gone wrong by start advertisements");
-	}
-
-	
-
-	// while(1){
-		// readADC();
-		//TODO add filter capabilities
-		// k_msleep(5000);
-		// k_yield();
-	// };
-
-
-        return 0;
-}
