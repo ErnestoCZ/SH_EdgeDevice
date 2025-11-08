cb577c322a3d5c42cca73b6e349e1ebb7f5ae4e8
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-11-08 17:41:34 +0100

    renamed function for char read and UUIDs

--- src/custom/cgatt.h
+++ src/custom/cgatt.h
@@ -5,4 +5,6 @@
 // create Service
 //  #define BT_UUID_TEMP_SERVICE BT_UUID_GATT_TEMP_STAT
 
-ssize_t read_temperature(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset);
+ssize_t read_temperature_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset);
+
+
