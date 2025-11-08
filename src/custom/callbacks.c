6fe4fd7724ae6ce2379c487dfa01821969fdd936
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-11-08 17:42:15 +0100

    more detailed cb handling

--- src/custom/callbacks.c
+++ src/custom/callbacks.c
@@ -1,7 +1,12 @@
 #include "callbacks.h"
+#include <zephyr/bluetooth/hci.h>
 
 LOG_MODULE_REGISTER(Callbacks, LOG_LEVEL_INF);
-
+extern struct bt_conn* my_conn;
+// extern struct bt_data ad[];
+// extern struct bt_data sd[];
+// extern struct bt_le_adv_param adv_param[];
+extern void advertising_start();
 struct bt_conn_cb bt_connection_callbacks = {
 	.connected = on_connect,
 	.disconnected = on_disconnect,
@@ -10,18 +15,42 @@ struct bt_conn_cb bt_connection_callbacks = {
 
 //TODO handling
 void on_connect(struct bt_conn *conn, uint8_t err){
-	if(!err){
-		LOG_INF("Connected by Device");
-	}else{
-		LOG_ERR("Connection error %s", err);
+	switch (err)
+	{
+	case BT_HCI_ERR_SUCCESS:
+		LOG_INF("Connected Device");
+		my_conn = bt_conn_ref(conn);
+		break;
+	case BT_HCI_ERR_UNKNOWN_CONN_ID:
+		LOG_ERR("Connection failed: BT_HCI_ERR_UNKNOWN_CONN_ID");
+		/* code */
+		break;
+	case BT_HCI_ERR_ADV_TIMEOUT:
+		LOG_ERR("Connection failed: BT_HCI_ERR_ADV_TIMEOUT");
+		break;
+	default:
+		break;
 	}
 
 };
 //TODO handling
 void on_disconnect(struct bt_conn *conn, uint8_t reason){
-	LOG_INF("Disconnected Device");
+	LOG_INF("Disconnected Device : Reason %d",reason);
+	switch (reason)
+	{
+	case BT_HCI_ERR_REMOTE_USER_TERM_CONN:
+		LOG_INF("Remote user terminated connection");
+		bt_conn_unref(my_conn);
+		/* code */
+		break;
+	
+	default:
+		bt_conn_unref(my_conn);
+		break;
+	}
 };
 
 void on_recycled(void){
-
+	LOG_INF("Recycling..");
+	advertising_start();
 }
