c9aed175b6064d2c7810c783e86cd6e63cebb2f7
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-12-04 21:12:57 +0100

    added functionality of DK buttons and leds without an implementation

--- src/custom/cgpio.c
+++ src/custom/cgpio.c
@@ -2,25 +2,97 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
-#define STATUS_LED_NODE DT_ALIAS(led_device_status)
 
 LOG_MODULE_REGISTER(cgpio, LOG_LEVEL_ERR);
 
-#ifdef DT_NODE_EXISTS(STATUS_LED_NODE)
-static const struct gpio_dt_spec led_device_status = GPIO_DT_SPEC_GET(STATUS_LED_NODE, gpios);
+#define STATUS_LED_NODE DT_NODELABEL(led0)
+#define STATUS_LED_ADV_NODE DT_NODELABEL(led1)
+#define BUTTON0 DT_NODELABEL(button0)
+#define BUTTON1 DT_NODELABEL(button1)
+#define BUTTON2 DT_NODELABEL(button2)
+#define BUTTON3 DT_NODELABEL(button3)
+
+// LEDs
+#if DT_NODE_EXISTS(STATUS_LED_ADV_NODE) && DT_NODE_EXISTS(STATUS_LED_NODE)
+static const struct gpio_dt_spec led_adv_status =
+    GPIO_DT_SPEC_GET(STATUS_LED_ADV_NODE, gpios);
+static const struct gpio_dt_spec led_device_status =
+    GPIO_DT_SPEC_GET(STATUS_LED_NODE, gpios);
 #endif
 
+// BUTTONS
+#if DT_NODE_EXISTS(BUTTON0) && DT_NODE_EXISTS(BUTTON1) &&                      \
+    DT_NODE_EXISTS(BUTTON2) && DT_NODE_EXISTS(BUTTON3)
+static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET(BUTTON0, gpios);
+static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET(BUTTON1, gpios);
+static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET(BUTTON2, gpios);
+static const struct gpio_dt_spec button3 = GPIO_DT_SPEC_GET(BUTTON3, gpios);
+#endif
 
-int init_gpio(void){
+static struct gpio_callback cb_button0;
+static struct gpio_callback cb_button1;
+static struct gpio_callback cb_button2;
+static struct gpio_callback cb_button3;
 
-    int ret = 0;
-    if(gpio_is_ready_dt(&led_device_status)){
-        ret = gpio_pin_configure_dt(&led_device_status,GPIO_OUTPUT | GPIO_ACTIVE_HIGH);
-        // gpio_pin_set_dt(&led_device_status,GPIO_OUTPUT_LOW);
-    }else{
-        return 1;
-    }
+void button0_callback(const struct device *port, struct gpio_callback *cb,
+                      gpio_port_pins_t pins) {};
+void button1_callback(const struct device *port, struct gpio_callback *cb,
+                      gpio_port_pins_t pins) {};
+void button2_callback(const struct device *port, struct gpio_callback *cb,
+                      gpio_port_pins_t pins) {};
+void button3_callback(const struct device *port, struct gpio_callback *cb,
+                      gpio_port_pins_t pins) {};
 
-    return 0;
+int init_gpio(void) {
 
+  int ret = 0;
+  
+  // LEDs
+  if (gpio_is_ready_dt(&led_device_status)) {
+    ret = gpio_pin_configure_dt(&led_device_status, GPIO_OUTPUT_INACTIVE);
+  } else {
+    return 1;
+  }
+  if (gpio_is_ready_dt(&led_adv_status)) {
+    ret = gpio_pin_configure_dt(&led_adv_status, GPIO_OUTPUT_INACTIVE);
+  } else {
+    return 1;
+  }
+
+
+  // Buttons
+  if (gpio_is_ready_dt(&button0)) {
+    ret = gpio_pin_configure_dt(&button0, GPIO_INPUT | GPIO_PULL_UP);
+    ret = gpio_pin_interrupt_configure_dt(&button0, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&cb_button0, button0_callback, BIT(button0.pin));
+    ret = gpio_add_callback_dt(&button0, &cb_button0);
+  } else {
+    return 1;
+  }
+  if (gpio_is_ready_dt(&button1)) {
+    ret = gpio_pin_configure_dt(&button1, GPIO_INPUT | GPIO_PULL_UP);
+    ret = gpio_pin_interrupt_configure_dt(&button1, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&cb_button1, button1_callback, BIT(button1.pin));
+    ret = gpio_add_callback_dt(&button1, &cb_button1);
+  } else {
+    return 1;
+  }
+  if (gpio_is_ready_dt(&button2)) {
+    ret = gpio_pin_configure_dt(&button2, GPIO_INPUT | GPIO_PULL_UP);
+    ret = gpio_pin_interrupt_configure_dt(&button2, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&cb_button2, button2_callback, BIT(button2.pin));
+    ret = gpio_add_callback_dt(&button2, &cb_button2);
+  } else {
+    return 1;
+  }
+  if (gpio_is_ready_dt(&button3)) {
+    ret = gpio_pin_configure_dt(&button3, GPIO_INPUT | GPIO_PULL_UP);
+    ret = gpio_pin_interrupt_configure_dt(&button3, GPIO_INT_EDGE_FALLING);
+    gpio_init_callback(&cb_button3, button3_callback, BIT(button3.pin));
+    ret = gpio_add_callback_dt(&button3, &cb_button3);
+  } else {
+    return 1;
+  }
+
+  return 0;
 };
